8f4beb7420fb57caf39733106c283524cb56af09
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-08-30 19:53:05 +0530

    added pipe

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ running = True
 
 class Bird:
     def __init__(self) -> None:
-        self.sprite = pygame.image.load('assets/bird.png')
+        self.sprite = pygame.transform.scale(pygame.image.load('assets/bird.png'), (32, 32))
         self.width = self.sprite.get_width()
         self.height = self.sprite.get_height()
         self.x = WIDTH/2-self.width/2
@@ -31,49 +31,64 @@ class Bird:
     def applyGravity(self):
         self.vel+=ACCELERATION * TIME_STEP
         self.y += self.vel * TIME_STEP
-        
-            
     
+    def resetPosition(self):
+        self.x = WIDTH/2-self.width/2
+        self.y = HEIGHT/2-self.height/2
+
     def checkCollision(self):
         if not self.y+self.height<=HEIGHT:
-            self.vel = -(self.vel)
+            self.vel = -(self.vel*0.5)
+            self.resetPosition()
         
         
     def draw(self):
-        # self.rect.top = self.y
-        # self.rect.left = self.x
         win.blit(self.sprite, (self.x, self.y))
 
     def fly(self):
-    
-        self.vel = -(self.vel + 150) * TIME_STEP
-        
-        # self.y -= 20
+        self.vel = -(self.vel + 100) * TIME_STEP
 
-        
+
+class Pipe:
+    def __init__(self, bird:Bird) -> None:
+        self.sprite = pygame.image.load('assets/pipe.png')
+        self.x = 0
+        self.y = 0
+        self.x_vel = 2
+        self.bird = bird
+
+    def draw(self):
+        win.blit(self.sprite, (self.x, self.y))
+
+    def checkCollision(self):
+        bird = self.bird
+        if bird.x + bird.width>self.x:
+            print('hello')
 
 flappyBird = Bird()
-
+pipe = Pipe(bird=flappyBird)
 while running:
     
     win.fill("white")
     flappyBird.draw()
+    pipe.draw()
 
     if not flappyBird.flying:
         flappyBird.applyGravity()
         flappyBird.checkCollision()
+        pipe.checkCollision()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_SPACE:
-        #         flappyBird.fly()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                flappyBird.fly()
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        flappyBird.fly()
+    # if keys[pygame.K_SPACE]:
+    #     flappyBird.fly()
     keys = pygame.key.get_pressed()
     
     
